469961f5ca42749b0062c643145d0586eef0b83b
Florian Heinz <fh@sysv.de>
2024-09-29 19:58:55 +0200

    Changed template

--- 3-SPL-Fibonacci/main.c
+++ 3-SPL-Fibonacci/main.c
@@ -8,16 +8,7 @@
 #include "include/gwindow.h"
 #include "include/simpio.h"
 
-
 int main(void) {
-  GWindow gw = newGWindow(600, 400);
-  pause(500);
-  setColor(gw, "orange");
-  fillOval(gw, 200, 100, 200, 200);
-
-  /* Wait for a mouse click, close the window and terminate program */
-  waitForClick();
-  closeGWindow(gw);
 
   return 0;
 }
