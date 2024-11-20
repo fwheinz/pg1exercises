cb51cdc6cb20507bfebeee14fae0d316f4e2b633
Florian Heinz <fh@sysv.de>
2024-11-20 22:08:20 +0000

    Fixed url

--- .devcontainer/extensions/oth/extension/extension.js
+++ .devcontainer/extensions/oth/extension/extension.js
@@ -8,7 +8,7 @@ const child = require("child_process");
 async function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand("oth.launchGUI", 
         () => {
-            let vncurl = `https://${process.env['CODESPACE_NAME']}-6080.${process.env['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN']}/`
+            let vncurl = `https://${process.env['CODESPACE_NAME']}-6080.${process.env['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN']}/vnc.html?autoconnect=true`
             vscode.env.openExternal(vscode.Uri.parse(vncurl))
         }
     ))
